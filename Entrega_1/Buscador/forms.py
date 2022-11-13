97a5b0730673abbaaf56a5e0f4babda5bd8ac3ac
Rogers Mezones <rogers.mezones@gmail.com>
2022-11-13 20:58:28 -0300

    se completa desafio, se agrega template de busqueda de mascota, se estilizan un poco los templates y se amplia contenido del readme

--- Entrega_1/Buscador/forms.py
+++ Entrega_1/Buscador/forms.py
@@ -5,17 +5,17 @@ class MascotaFormulario(forms.Form):
     nombre_mascota = forms.CharField(max_length=20)
     raza = forms.CharField(max_length=20)
     color = forms.CharField(max_length=20)
-    fecha_nacimiento = forms.DateField()
+    fecha_nacimiento = forms.DateField(help_text="AAAA-MM-DD")
 
 
 class CriadorFormulario(forms.Form):
     nombre_criador = forms.CharField(max_length=30)
     apellido = forms.CharField(max_length=30)
     provincia = forms.CharField(max_length=30)
-    fecha_nacimiento = forms.DateField()
+    fecha_nacimiento = forms.DateField(help_text="AAAA-MM-DD")
 
 class AlimentoFormulario(forms.Form):
     sabor = forms.CharField(max_length=20)
     raza = forms.CharField(max_length=20)
-    tamaño = forms.CharField(max_length=20)
+    tamaño = forms.CharField(max_length=20, help_text="1/2 Kilo, 1 kilo, 4 Kilo, etc")
 
